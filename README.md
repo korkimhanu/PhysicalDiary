b00196c16a6b648ba5cf899193d1b6048c0fdd01
korkimhanu <k-j-m1234@naver.com>
2023-10-27 01:57:29 +0900

    Main Loby UI update

--- README.md
+++ README.md
@@ -11,11 +11,20 @@
 >git push origin master ::깃헙에 업로드
 >```
 >
->### 나. 별첨
+>### 나. 코드 위치
+> + [main code](main.py)
+> + [UIs](UI)
+>   + [MainScreen(Loby)](UI/ui_mainScreen.py)
+> + [UI editor](venv/Scripts/pyside6-designer.exe) (UI를 코딩이 아닌 UI가 있는 에디터로 편집하고 싶을 떄 사용)
 > 
->### 다. pyside6 & PyQt5 관련 강의 모음
+>### 다. pyside6 & PyQt5 & git 관련 강의 모음
 >1. [전반적인 강의](https://answer-me.tistory.com/category/pyside6)
 >2. [시리얼모니터만들기](https://sc.sogang.ac.kr/bbs/bbsview.do?pkid=69555&bbsid=3857&wslID=mecha&searchField=&searchValue=&currentPage=1)
 >3. [유튜브강의(pyQt5)](https://youtube.com/playlist?list=PL1eLKSeW1Baj72go6l3gg4C8TXRNUBdMo&si=sX0lmVDnQGDhhgUW)
 >4. [유튜브강의(pyslide2)](https://www.youtube.com/playlist?list=PL1eLKSeW1Bah_G3DnTwQiytpMlET04LN7)
 >5. [유튜브강의(pyslide6)](https://youtube.com/playlist?list=PLl0_N0yeFLesyK9nHoww-vL5As3aPlWgq&si=lxE_QT6QE4fkCmZd)
+>6. [git에서 branch란?](https://git-scm.com/book/ko/v2/Git-%EB%B8%8C%EB%9E%9C%EC%B9%98-%EB%B8%8C%EB%9E%9C%EC%B9%98%EB%9E%80-%EB%AC%B4%EC%97%87%EC%9D%B8%EA%B0%80)
+>7. [label 이미지 설정](https://martinii.fun/192)
+>
+> 
+> [readme.md 수정방법](https://gist.github.com/ihoneymon/652be052a0727ad59601)
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
+from UI.ui_mainScreen import Ui_MainWindow #로비 UI를 UI 디렉토리에서 import
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Loby_Window(QMainWindow, Ui_MainWindow):  #로비 화면 클래스 생성
+    def __init__(self): #class 기본 인자 설정
+        super(Loby_Window,self).__init__()
+        self.setupUi(self)
 
+app = QApplication(sys.argv)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyChardddm')
+window = Loby_Window() #Loby_window 클래스 변수 생성
+window.setFixedSize(1080,720) #창 크기 1080*720으로 설정
+window.show() #창 띄우기
 
-    print("Hello World!") # Git push test by Sleepy Cho
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.exec() #앱 실행
