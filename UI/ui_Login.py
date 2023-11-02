e14447bc66e4c4943966379d7041fd23213e9d63
houououou <houuu290@gmail.com>
2023-11-02 19:15:42 +0900

    LoginUI

---
+++ UI/ui_Login.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'untitled2dMFDRa.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QPushButton,
+    QSizePolicy, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(806, 600)
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(250, 200, 101, 61))
+        self.lineEdit_id = QLineEdit(Form)
+        self.lineEdit_id.setObjectName(u"lineEdit_id")
+        self.lineEdit_id.setGeometry(QRect(350, 210, 113, 31))
+        self.pushButton_ed = QPushButton(Form)
+        self.pushButton_ed.setObjectName(u"pushButton_ed")
+        self.pushButton_ed.setGeometry(QRect(590, 480, 131, 61))
+        self.lineEdit_pw = QLineEdit(Form)
+        self.lineEdit_pw.setObjectName(u"lineEdit_pw")
+        self.lineEdit_pw.setGeometry(QRect(350, 270, 113, 31))
+        self.label_3 = QLabel(Form)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(240, 260, 101, 61))
+        self.label_2 = QLabel(Form)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(310, 40, 121, 51))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label.setText(QCoreApplication.translate("Form", u"<html><head/><body><p align=\"center\"><span style=\" font-size:14pt;\">\uc544\uc774\ub514</span></p></body></html>", None))
+        self.pushButton_ed.setText(QCoreApplication.translate("Form", u"\uc885\ub8cc", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"<html><head/><body><p align=\"center\"><span style=\" font-size:14pt;\">\ube44\ubc00\ubc88\ud638</span></p></body></html>", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"<html><head/><body><p align=\"center\"><span style=\" font-size:18pt;\">\ub85c\uadf8\uc778</span></p></body></html>", None))
+    # retranslateUi
+
